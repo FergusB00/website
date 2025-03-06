e16773aced00e1a2826a97f2bf62709de63776a1
Fergus Buscall <buscallf@gmail.com>
2025-03-06 10:16:43 +0000

    Back to top button styling

--- app/javascript/controllers/back_to_top_controller.js
+++ app/javascript/controllers/back_to_top_controller.js
@@ -1,1 +1,19 @@
 import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    window.addEventListener("scroll", this.toggleButton.bind(this));
+  }
+
+  toggleButton() {
+    if (window.scrollY > 300) {
+      this.element.style.display = "block";
+    } else {
+      this.element.style.display = "none";
+    }
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+}
